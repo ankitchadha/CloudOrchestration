4bc5c8ad14508c6c4cb5a84f34b27300db3a4d92
ankitchadha <ankitchadha29@gmail.com>
2020-09-08 16:25:02 -0700

    Update AWS-ELB-ASG.tf
    Update instances' SG to allow outbound communication.

--- Terraform/AWS-ELB-ASG.tf
+++ Terraform/AWS-ELB-ASG.tf
@@ -17,6 +17,12 @@ resource "aws_security_group" "allow-http" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     lifecycle {
         create_before_destroy = true
     }
